9e4afb897c04c05cd097ab3ec8e790c3db2b27b4
Michael Turner <turner.michael923@gmail.com>
2025-03-16 21:48:26 -0500

    version 2

--- project_app.py
+++ project_app.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
+import numpy as np
 
-df = pd.read_csv('vehicles_us.csv', low_memory = False)
-#df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
-print(df.info())
+df = pd.read_csv('C:/Users/turne/Project4/vehicles_us.csv')
 
-#st.header('Data Viewer')
-#st.dataframe(df)
+df['cylinders'] = pd.to_numeric(df['cylinders'])
+df['cylinders'] = df['cylinders'].astype('Int64')
+df['date_posted'] = pd.to_datetime(df['date_posted'])
 
-#st.header('Vehicle types by manufacturer')
-# create a plotly histogram figure
-#fig = px.histogram(df, x='manufacturer', color='type')
-# display the figure with streamlit
-#st.write(fig)
+df['model_year'] = df['model_year'].fillna('Unknown')
+df['cylinders'] = df['cylinders'].fillna(0)
+df['odometer'] = df['odometer'].fillna('NaN')
+df['paint_color'] = df['paint_color'].fillna('Unknown')
+df['is_4wd'] = df['is_4wd'].replace(np.nan, 0).astype(bool)
+
+st.header('4WD vs Non-4WD Vehicle Sale Time')
+fig = px.histogram(df, x='days_listed', color='is_4wd', title='Sale Time for 4WD vs Non-4WD Vehicles',
+                   labels={'days_listed': 'Days Listed', 'is_4wd': '4WD Vehicle'},
+                   nbins=30)
+st.write(fig)
